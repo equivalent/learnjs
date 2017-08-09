c4e0038922040e6a65dc8d09dc8042af30aa989c
Tomas Valent <equivalent@eq8.eu>
2017-08-09 09:26:02 +0200

    result evaluation + visual feedback

--- public/app.js
+++ public/app.js
@@ -2,11 +2,54 @@
 
 var learnjs = {};
 
+learnjs.problems = [
+  {
+    description: "What is truth?",
+    code: "function problem() { return __; }"
+  },
+  {
+    description: "Simple Math",
+    code: "function problem() { return 42 === 6 * __; }"
+  }
+];
+
+learnjs.applyObject = function(obj, elem) {
+  for (var key in obj) {
+    elem.find('[data-name="' + key + '"]').text(obj[key]);
+  }
+};
+
+learnjs.flashElement = function(elem, content) {
+  elem.fadeOut('fast', function() {
+    elem.html(content);
+    elem.fadeIn();
+  });
+}
+
 learnjs.problemView = function(data) {
   var problemNumber = parseInt(data, 10);
   var view = $('.templates .problem-view').clone();
+  var problemData = learnjs.problems[problemNumber - 1];
+  var resultFlash = view.find('.result');
+
+  function checkAnswer() { 
+    var answer = view.find('.answer').val();
+    var test = problemData.code.replace('__', answer) + '; problem();';
+    return eval(test);
+  }
+
+  function checkAnswerClick() {
+    if (checkAnswer()) {
+      learnjs.flashElement(resultFlash, 'Correct!');
+    } else {
+      learnjs.flashElement(resultFlash, 'Incorrect!');
+    }
+    return false; 
+  }
+
+  view.find('.check-btn').click(checkAnswerClick);
   view.find('.title').text('Problem #' + problemNumber);
-  learnjs.applyObject(learnjs.problems[problemNumber - 1], view);
+  learnjs.applyObject(problemData, view);
   return view;
 }
 
@@ -27,20 +70,3 @@ learnjs.appOnReady = function() {
   };
   learnjs.showView(window.location.hash);
 }
-
-learnjs.applyObject = function(obj, elem) {
-  for (var key in obj) {
-    elem.find('[data-name="' + key + '"]').text(obj[key]);
-  }
-};
-
-learnjs.problems = [
-  {
-    description: "What is truth?",
-    code: "function problem() { return __; }"
-  },
-  {
-    description: "Simple Math",
-    code: "function problem() { return 42 === 6 * __; }"
-  }
-];
